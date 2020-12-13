a36a47dc70e793f004f297ad6d2a4712886817f7
bipan82gill <56560207+bipan82gill@users.noreply.github.com>
2020-12-12 22:19:03 -0500

    setup server and mongo connection

---
+++ backend/server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const morgan = require("morgan");
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const app = express();
+//database
+mongoose.connect(process.env.DATABASE_CLOUD,{useNewUrlParser:true, useCreateIndex: true,useFindAndModify:false}).then(()=>console.log('DB connected'));
+
+
+app.use(morgan('dev'));
+app.use(bodyParser.json())
+app.use(cookieParser())
+
+//cors
+if(process.env.NODE_ENV === 'development'){
+app.use(cors({ origin:`${process.env.CLIENT_URL}`}));
+}
+//routes
+app.get('/api', (req, res)=>{
+    res.json({time: Date().toString()});
+})
+
+//port 
+
+const port = process.env.PORT || 8000;
+
+app.listen(port, ()=> console.log(`server is running on port ${port}`));
