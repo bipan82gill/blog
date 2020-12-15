4ea27b554a3705852b146bdf989212f66f4991fd
bipan82gill <56560207+bipan82gill@users.noreply.github.com>
2020-12-15 17:11:24 -0500

    create nav bar

--- backend/Routes/auth.js
+++ backend/Routes/auth.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const {signup, signin} = require('../controllers/auth');
+const {signup, signin, signout, requireSignin} = require('../controllers/auth');
 
 //validators
 
@@ -9,4 +9,11 @@ const {userSignupValidator, userSigninValidator} = require('../validators/auth')
 
 router.post('/signup',userSignupValidator, runValidation, signup);
 router.post('/signin',userSigninValidator, runValidation, signin);
+router.get('/signout', signout);
+
+router.get('/secret', requireSignin, (req, res)=>{
+    res.json({
+        message: 'you have access to secret page'
+    })
+})
 module.exports = router;
