b33d93f570bf7343152b77654078d694e8f0740b
bipan82gill <56560207+bipan82gill@users.noreply.github.com>
2020-12-21 15:39:10 -0500

    build signup page

--- backend/server.js
+++ backend/server.js
@@ -20,15 +20,15 @@ mongoose
 app.use(morgan('dev'));
 app.use(bodyParser.json())
 app.use(cookieParser())
-app.use('/api',blogRoutes);
-app.use('/api', authRoutes);
+
 
 //cors
 if(process.env.NODE_ENV === 'development'){
 app.use(cors({ origin:`${process.env.CLIENT_URL}`}));
 }
 //port 
-
+app.use('/api',blogRoutes);
+app.use('/api', authRoutes);
 const port = process.env.PORT || 8000;
 
 app.listen(port, ()=> console.log(`server is running on port ${port}`));
