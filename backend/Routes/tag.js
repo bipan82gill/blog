1c0c71a324c8894fad6b2c1c3b4da1de0f6c38b3
bipan82gill <56560207+bipan82gill@users.noreply.github.com>
2020-12-30 14:41:18 -0500

    insert nprogress css

--- backend/Routes/tag.js
+++ backend/Routes/tag.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 const {requireSignin, adminMiddleware}= require('../controllers/auth');
+const {create, list, read, remove}  = require('../controllers/tag');
 
 
 const {runValidation} = require('../validators');
@@ -10,6 +11,6 @@ const {createTagValidator} = require('../validators/tag');
 router.post('/tag', createTagValidator, runValidation, requireSignin, adminMiddleware, create);
 router.get('/tags', list);
 router.get('/tag/:slug', read);
-router.remove('/tag/:slug', requireSignin, adminMiddleware, remove);
+router.delete('/tag/:slug', requireSignin, adminMiddleware, remove);
 
 module.exports = router;
--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -1,7 +1,9 @@
 import React, {useState} from 'react';
 import {APP_NAME} from '../config';
 import Router from 'next/router';
+import NProgress from 'nprogress';
 import Link from 'next/link';
+
 import {signout, isAuth} from '../actions/auth';
 import {
   Collapse,
@@ -17,6 +19,13 @@ import {
   DropdownItem,
   NavbarText
 } from 'reactstrap';
+import '.././node_modules/nprogress/nprogress.css';
+
+Router.onRouteChangeStart = url => NProgress.start()
+
+Router.onRouteChangeComplete = url => NProgress.done()
+
+Router.onRouteChangeError = url => NProgress.done()
 
 const Header = () =>{
     const [isOpen, setIsOpen] = useState(false);
